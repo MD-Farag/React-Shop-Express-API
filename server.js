032f175719aa100780eece29edffc19be8033f4a
Mohammad Nasser <99610613+MD-Farag@users.noreply.github.com>
2025-02-11 09:06:16 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import header from "./routes/header.js";
 import homeProducts from "./routes/homeProducts.js";
 
 const app = express();
+const cors = require("cors");
+
 
 // Default to process.env.PORT or 5000 for local development
 const PORT = process.env.PORT || 5000;
@@ -12,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 // Body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000" }));
 
 // Setup static folder
 app.use(express.static("assets"));
