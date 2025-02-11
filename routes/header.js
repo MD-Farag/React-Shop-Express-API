799fe87307c777113544438f61ead2aff633740e
Mohammad Nasser <99610613+MD-Farag@users.noreply.github.com>
2025-02-11 08:12:58 +0300

    Add files via upload

--- routes/header.js
+++ routes/header.js
@@ -1,82 +1,124 @@
-import express from 'express'
-const router = express.Router()
-import header from '../data/header.json' assert { type: "json" };
+import express from "express";
+const router = express.Router();
+import { readFile } from "fs";
 
 // Get all men
-router.get('/men', (req, res) => {
-    const limit = req.query.limit
-    if (!isNaN(limit) && limit > 0) {
-        return res.status(200).json(header.men.slice(0, limit))
-    }
-    res.status(200).json(header.men)
-})
+router.get("/men", (req, res) => {
+	getAllItems(req, "./data/header.json", res, "men");
+});
 
 // Get all women
-router.get('/women', (req, res) => {
-    const limit = req.query.limit
-    if (!isNaN(limit) && limit > 0) {
-        return res.status(200).json(header.women.slice(0, limit))
-    }
-    res.status(200).json(header.women)
-})
+router.get("/women", (req, res) => {
+	getAllItems(req, "./data/header.json", res, "women");
+});
 
 // Get all kids
-router.get('/kids', (req, res) => {
-    const limit = req.query.limit
-    if (!isNaN(limit) && limit > 0) {
-        return res.status(200).json(header.kids.slice(0, limit))
-    }
-    res.status(200).json(header.kids)
-})
+router.get("/kids", (req, res) => {
+	getAllItems(req, "./data/header.json", res, "kids");
+});
 
 // Get all slides
-router.get('/slides', (req, res) => {
-    const limit = req.query.limit
-    if (!isNaN(limit) && limit > 0) {
-        return res.status(200).json(header.slides.slice(0, limit))
-    }
-    res.status(200).json(header.slides)
-})
+router.get("/slides", (req, res) => {
+	getAllItems(req, "./data/header.json", res, "slides");
+});
 
 // Get all products in summer collection category slide
-router.get('/summerCollection', (req, res) => {
-    console.log("summer url", req.originalUrl)
-    const limit = req.query.limit
-    if (!isNaN(limit) && limit > 0) {
-        return res.status(200).json(header.summerCollection.slice(0, limit))
-    }
-    res.status(200).json(header.summerCollection)
-    
-})
+router.get("/summerCollection", (req, res) => {
+	getAllItems(req, "./data/header.json", res, "summerCollection");
+});
 
 // Get all products in winter clearance category slide
-router.get('/winterClearance', (req, res) => {
-    console.log("winter url", req.originalUrl)
-    const limit = req.query.limit
-    if (!isNaN(limit) && limit > 0) {
-        return res.status(200).json(header.winterClearance.slice(0, limit))
-    }
-    res.status(200).json(header.winterClearance)
-    
-})
+router.get("/winterClearance", (req, res) => {
+	getAllItems(req, "./data/header.json", res, "winterClearance");
+});
 
 // Get all headerFeatures
-router.get('/headerFeatures', (req, res) => {
-    const limit = req.query.limit
-    if (!isNaN(limit) && limit > 0) {
-        return res.status(200).json(header.headerFeatures.slice(0, limit))
-    }
-    res.status(200).json(header.headerFeatures)
-})
+router.get("/headerFeatures", (req, res) => {
+	getAllItems(req, "./data/header.json", res, "headerFeatures");
+});
 
+const getAllItems = (req, file, res, list) => {
+	const limit = parseInt(req.query.limit);
+	let products = [];
+	readFile(file, (err, data) => {
+		if (!err) {
+			products = JSON.parse(data.toString());
+			if (!isNaN(limit) && limit > 0) {
+				return res.status(200).json(products[list].slice(0, limit));
+			}
+			res.status(200).json(products[list]);
+		}
+		// console.log('error is: ', err)
+	});
+};
 
+////////========= Old Code =======//////////////
+// Get all men
+// router.get("/men", (req, res) => {
+// 	const limit = req.query.limit;
+// 	if (!isNaN(limit) && limit > 0) {
+// 		return res.status(200).json(header.men.slice(0, limit));
+// 	}
+// 	res.status(200).json(header.men);
+// });
 
+// // Get all women
+// router.get("/women", (req, res) => {
+// 	const limit = req.query.limit;
+// 	if (!isNaN(limit) && limit > 0) {
+// 		return res.status(200).json(header.women.slice(0, limit));
+// 	}
+// 	res.status(200).json(header.women);
+// });
 
+// // Get all kids
+// router.get("/kids", (req, res) => {
+// 	const limit = req.query.limit;
+// 	if (!isNaN(limit) && limit > 0) {
+// 		return res.status(200).json(header.kids.slice(0, limit));
+// 	}
+// 	res.status(200).json(header.kids);
+// });
 
+// // Get all slides
+// router.get("/slides", (req, res) => {
+// 	const limit = req.query.limit;
+// 	if (!isNaN(limit) && limit > 0) {
+// 		return res.status(200).json(header.slides.slice(0, limit));
+// 	}
+// 	res.status(200).json(header.slides);
+// });
 
+// // Get all products in summer collection category slide
+// router.get("/summerCollection", (req, res) => {
+// 	console.log("summer url", req.originalUrl);
+// 	const limit = req.query.limit;
+// 	if (!isNaN(limit) && limit > 0) {
+// 		return res.status(200).json(header.summerCollection.slice(0, limit));
+// 	}
+// 	res.status(200).json(header.summerCollection);
+// });
 
+// // Get all products in winter clearance category slide
+// router.get("/winterClearance", (req, res) => {
+// 	console.log("winter url", req.originalUrl);
+// 	const limit = req.query.limit;
+// 	if (!isNaN(limit) && limit > 0) {
+// 		return res.status(200).json(header.winterClearance.slice(0, limit));
+// 	}
+// 	res.status(200).json(header.winterClearance);
+// });
 
+// // Get all headerFeatures
+// router.get("/headerFeatures", (req, res) => {
+// 	const limit = req.query.limit;
+// 	if (!isNaN(limit) && limit > 0) {
+// 		return res.status(200).json(header.headerFeatures.slice(0, limit));
+// 	}
+// 	res.status(200).json(header.headerFeatures);
+// });
 
+///////////======= Very Old  Code ========///////////
 // Get single post
 // router.get('/:id', (req, res) => {
 //     const id = parseInt(req.params.id)
@@ -129,6 +171,5 @@ router.get('/headerFeatures', (req, res) => {
 //     header = header.filter((post) => post.id !== id)
 //     res.status(200).json(header)
 // })
-
 
 export default router;
--- routes/homeProducts.js
+++ routes/homeProducts.js
@@ -1,87 +1,108 @@
 import express from "express";
-const router = express.Router()
-import { readFile } from 'fs'
+const router = express.Router();
+import { readFile } from "fs";
 
 // Get all selectedItems
-router.get('/selectedItems', (req, res) => { getAllItems(req, './data/homeProducts.json', res, 'selectedItems') })
+router.get("/selectedItems", (req, res) => {
+	getAllItems(req, "./data/homeProducts.json", res, "selectedItems");
+});
 
 // Get single item from selectedItems
-router.get('/selectedItems/:id', (req, res) => { getSingleItem(req, './data/homeProducts.json', res, 'selectedItems') })
+router.get("/selectedItems/:id", (req, res) => {
+	getSingleItem(req, "./data/homeProducts.json", res, "selectedItems");
+});
 
 // Get all defaultSale
-router.get('/defaultSale', (req, res) => { getAllItems(req, './data/homeProducts.json', res, 'defaultSale') })
+router.get("/defaultSale", (req, res) => {
+	getAllItems(req, "./data/homeProducts.json", res, "defaultSale");
+});
 
 // Get single item from defaultSale
-router.get('/defaultSale/:id', (req, res) => { getSingleItem(req, './data/homeProducts.json', res, 'defaultSale') })
+router.get("/defaultSale/:id", (req, res) => {
+	getSingleItem(req, "./data/homeProducts.json", res, "defaultSale");
+});
 
 // Get all newArrivals
-router.get('/newArrivals', (req, res) => { getAllItems(req, './data/homeProducts.json', res, 'newArrivals') })
+router.get("/newArrivals", (req, res) => {
+	getAllItems(req, "./data/homeProducts.json", res, "newArrivals");
+});
 
 // Get single item from newArrivals
-router.get('/newArrivals/:id', (req, res) => { getSingleItem(req, './data/homeProducts.json', res, 'newArrivals') })
+router.get("/newArrivals/:id", (req, res) => {
+	getSingleItem(req, "./data/homeProducts.json", res, "newArrivals");
+});
 
 // Get all basic
-router.get('/basic', (req, res) => { getAllItems(req, './data/homeProducts.json', res, 'basic') })
+router.get("/basic", (req, res) => {
+	getAllItems(req, "./data/homeProducts.json", res, "basic");
+});
 
 // Get single item from basic
-router.get('/basic/:id', (req, res) => { getSingleItem(req, './data/homeProducts.json', res, 'basic') })
+router.get("/basic/:id", (req, res) => {
+	getSingleItem(req, "./data/homeProducts.json", res, "basic");
+});
 
 // Get all sale
-router.get('/sale', (req, res) => { getAllItems(req, './data/homeProducts.json', res, 'sale') })
+router.get("/sale", (req, res) => {
+	getAllItems(req, "./data/homeProducts.json", res, "sale");
+});
 
 // Get single item from sale
-router.get('/sale/:id', (req, res) => { getSingleItem(req, './data/homeProducts.json', res, 'sale') })
+router.get("/sale/:id", (req, res) => {
+	getSingleItem(req, "./data/homeProducts.json", res, "sale");
+});
 
 // Get all todayItems
-router.get('/todayItems', (req, res) => { getAllItems(req, './data/homeProducts.json', res, 'todayItems') })
+router.get("/todayItems", (req, res) => {
+	getAllItems(req, "./data/homeProducts.json", res, "todayItems");
+});
 
 // Get single item from todayItems
-router.get('/todayItems/:id', (req, res) => { getSingleItem(req, './data/homeProducts.json', res, 'todayItems') })
+router.get("/todayItems/:id", (req, res) => {
+	getSingleItem(req, "./data/homeProducts.json", res, "todayItems");
+});
 
 // Get all whyUs
-router.get('/whyUs', (req, res) => { getAllItems(req, './data/homeProducts.json', res, 'whyUs') })
+router.get("/whyUs", (req, res) => {
+	getAllItems(req, "./data/homeProducts.json", res, "whyUs");
+});
 
 // Get single item from todayItems
-router.get('/whyUs/:id', (req, res) => { getSingleItem(req, './data/homeProducts.json', res, 'whyUs') })
-
-
-
-
+router.get("/whyUs/:id", (req, res) => {
+	getSingleItem(req, "./data/homeProducts.json", res, "whyUs");
+});
 
 const getAllItems = (req, file, res, list) => {
-    const limit = parseInt(req.query.limit)
-    // console.log(req.originalUrl)
-    let products = []
-    readFile(file, (err, data) => {
-        if (!err) {
-            products = JSON.parse(data.toString())
-            if (!isNaN(limit) && limit > 0) {
-                return res.status(200).json(products[list].slice(0, limit))
-            }
-            res.status(200).json(products[list])
-        }
-        // console.log('error is: ', err)
-    })
-}
+	const limit = parseInt(req.query.limit);
+	// console.log(req.originalUrl)
+	let products = [];
+	readFile(file, (err, data) => {
+		if (!err) {
+			products = JSON.parse(data.toString());
+			if (!isNaN(limit) && limit > 0) {
+				return res.status(200).json(products[list].slice(0, limit));
+			}
+			res.status(200).json(products[list]);
+		}
+		// console.log('error is: ', err)
+	});
+};
 
 const getSingleItem = (req, file, res, list) => {
-    const id = parseInt(req.params.id)
-    let products = []
-    // console.log(req.originalUrl)
-    readFile(file, (err, data) => {
-        if (!err) {
-            products = JSON.parse(data.toString())
-            const product = products[list].find((product) => product.id == id)
-            if (!product) {
-                return res
-                    .status(404)
-                    .json({ msg: `A product with id ${id} was not found` })
-            }
-            res.status(200).json(product)
-        }
-        // console.log('error is: ', err)
-    })
-}
+	const id = parseInt(req.params.id);
+	let products = [];
+	// console.log(req.originalUrl)
+	readFile(file, (err, data) => {
+		if (!err) {
+			products = JSON.parse(data.toString());
+			const product = products[list].find((product) => product.id == id);
+			if (!product) {
+				return res.status(404).json({ msg: `A product with id ${id} was not found` });
+			}
+			res.status(200).json(product);
+		}
+		// console.log('error is: ', err)
+	});
+};
 
-
-export default router
+export default router;
